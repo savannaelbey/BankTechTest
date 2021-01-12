33df61b19eb836723e0b115050b5cf36fb2f5612
savannaelbey <savanna_e@icloud.com>
2021-01-12 12:30:35 +0000

    Add test for Account deposit transaction creation feature and pass
    Wrote test to ensure that when an account is instantiated it has an empty list of transactionHisotry and that everytime the deposit method is called a new transaction is instantiated and pushed into the transactionHisory array.

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,9 +1,28 @@
 'use strict';
 
-describe('Account', function () {
-  it('allows user to deposit money', function () {
-    let myAccount = new Account();
-    myAccount.deposit(500)
-    expect(myAccount.getBalance()).toEqual(500);
+describe('Account class', function () {
+  let myAccount;
+
+  beforeEach(function() {
+    myAccount = new Account()
   });
+
+  describe('deposit() method', function () {
+    it('allows user to deposit money', function () {
+      myAccount;
+      myAccount.deposit(500)
+      expect(myAccount.getBalance()).toEqual(500);
+    });
+
+    it('adds an instance of transaction to the transaction history', function () {
+      myAccount;
+      expect(myAccount.getTransactionHistory().length).toEqual(0);
+      myAccount.deposit(500);
+      expect(myAccount.getTransactionHistory().length).toEqual(1);
+      expect(myAccount.getTransactionHistory()[0]).toBeInstanceOf(Transaction)
+    });
+
+  });
+
+
 });
--- src/account.js
+++ src/account.js
@@ -2,15 +2,22 @@
 
 class Account {
 
-  constructor(balance = 0) {
+  constructor(balance = 0, transactionHistory = []) {
     this.balance = balance;
+    this.transactionHistory = transactionHistory;
   }
 
   getBalance() {
     return this.balance;
   }
 
+  getTransactionHistory() {
+    return this.transactionHistory;
+  }
+
   deposit(amount) {
     this.balance += amount;
+    let depositTransaction = new Transaction();
+    this.transactionHistory.push(depositTransaction);
   }
 }
