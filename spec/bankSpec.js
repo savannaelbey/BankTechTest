60cff12c26d059b169560f9d398a1c8f1b4d7ed3
savannaelbey <savanna_e@icloud.com>
2021-01-18 00:25:52 +0000

    refactor Bank class and associated unit tests

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -23,26 +23,26 @@ describe('Bank', function () {
     jasmine.clock().uninstall();
   });
 
-  describe('printStatement(account) method', function () {
+  describe('printStatement method', function () {
     it('shows the transaction date', function () {
       accountDouble;
       let transactionDouble = new TransactionDouble(date);
       accountDouble.deposit(500, transactionDouble);
-      expect(bank.printStatement(accountDouble)).toContain('13/1/2021');
+      expect(bank.printStatement()).toContain('13/1/2021');
     });
 
     it('shows deposited amount', function () {
       accountDouble;
       let transactionDouble = new TransactionDouble(date);
       accountDouble.deposit(500, transactionDouble);
-      expect(bank.printStatement(accountDouble)).toContain('500');
+      expect(bank.printStatement()).toContain('500');
     });
 
     it('shows withdrawn amount', function () {
       accountDouble;
       let transactionDouble = new TransactionDouble(date);
       accountDouble.withdraw(250, transactionDouble);
-      expect(bank.printStatement(accountDouble)).toContain('250');
+      expect(bank.printStatement()).toContain('250');
     });
 
     it('shows balance after amount is deposited', function () {
@@ -51,7 +51,7 @@ describe('Bank', function () {
       accountDouble.deposit(500, transactionDouble);
       let transactionDouble2 = new TransactionDouble(date);
       accountDouble.deposit(250, transactionDouble2);
-      expect(bank.printStatement(accountDouble)).toContain(750);
+      expect(bank.printStatement()).toContain(750);
     });
 
     it('shows remaining balance after amount is withdrawn', function () {
@@ -60,26 +60,26 @@ describe('Bank', function () {
       accountDouble.deposit(1000, transactionDouble);
       let transactionDouble2 = new TransactionDouble(date);
       accountDouble.withdraw(250, transactionDouble2);
-      expect(bank.printStatement(accountDouble)).toContain(750);
+      expect(bank.printStatement()).toContain(750);
     });
 
     it('outputs all the data of a deposit transaction in the correct format', function () {
       accountDouble;
       let transactionDouble = new TransactionDouble(date);
       accountDouble.deposit(500, transactionDouble);
-      expect(bank.printStatement(accountDouble)).toContain('13/1/2021 || 500.00 ||  || 500.00' );
+      expect(bank.printStatement()).toContain('13/1/2021 || 500.00 ||  || 500.00' );
     });
 
     it('outputs all the data of a withdrawal transaction in the correct format', function () {
       accountDouble;
       let transactionDouble = new TransactionDouble(date);
       accountDouble.withdraw(250, transactionDouble);
-      expect(bank.printStatement(accountDouble)).toContain('13/1/2021 ||  || 250.00 || -250.00');
+      expect(bank.printStatement()).toContain('13/1/2021 ||  || 250.00 || -250.00');
     });
 
     it('shows a column header', function () {
       accountDouble;
-      expect(bank.printStatement(accountDouble)).toContain('date || credit || debit || balance\n')
+      expect(bank.printStatement()).toContain('date || credit || debit || balance\n')
     });
 
     it('shows the details of each of the transactions in the account transaction history in the correct order', function() {
@@ -88,7 +88,7 @@ describe('Bank', function () {
       accountDouble.deposit(400, transactionDouble);
       let transactionDouble2 = new TransactionDouble(date);
       accountDouble.withdraw(100, transactionDouble2);
-      expect(bank.printStatement(accountDouble)).toContain('13/1/2021 ||  || 100.00 || 300.00' + '\n' + '13/1/2021 || 400.00 ||  || 400.00');
+      expect(bank.printStatement()).toContain('13/1/2021 ||  || 100.00 || 300.00' + '\n' + '13/1/2021 || 400.00 ||  || 400.00');
     });
 
     it('shows the statement in the required format', function () {
@@ -99,7 +99,7 @@ describe('Bank', function () {
       accountDouble.withdraw(250, transactionDouble2);
       let transactionDouble3 = new TransactionDouble(date);
       accountDouble.deposit(500, transactionDouble3);
-      expect(bank.printStatement(accountDouble)).toEqual('date || credit || debit || balance' + '\n' + '13/1/2021 || 500.00 ||  || 750.00' + '\n' + '13/1/2021 ||  || 250.00 || 250.00' + '\n' + '13/1/2021 || 500.00 ||  || 500.00')
+      expect(bank.printStatement()).toEqual('date || credit || debit || balance' + '\n' + '13/1/2021 || 500.00 ||  || 750.00' + '\n' + '13/1/2021 ||  || 250.00 || 250.00' + '\n' + '13/1/2021 || 500.00 ||  || 500.00')
     });
   });
 });
